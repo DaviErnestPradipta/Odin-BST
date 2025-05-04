282261e3c1a51ab9060cc64dfda0f93998f9600d
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-05-04 17:23:21 +0900

    Add insert function

--- BST.js
+++ BST.js
@@ -13,6 +13,18 @@ export default class BST {
         const newNode = Node(sortedArray[midpoint]);
         newNode.leftChild = this.buildTree(sortedArray.slice(0, midpoint));
         newNode.rightChild = this.buildTree(sortedArray.slice(midpoint + 1));
+        
         return newNode;
+    }
+
+    insert(value, currentNode = this.root) {
+        if (currentNode === null) return Node(value);
+        if (currentNode.value === value) return;
+
+        if (currentNode.value < value)
+            currentNode.rightChild = this.insert(value, currentNode.rightChild);
+        else currentNode.leftChild = this.insert(value, currentNode.leftChild);
+
+        return currentNode;
     }
 }
